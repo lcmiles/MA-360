120b0dbc23fb34e4a3be186195843c46dae09eec
lcmiles <lcmiles@uab.edu>
2024-02-19 11:03:35 -0600

    Pushing changes

--- Projects/Project2/Project2.m
+++ Projects/Project2/Project2.m
@@ -1,6 +1,19 @@
+%Composition 1 with Read and Blue
+
+figure('Name','Composition 1 with Read and Blue');
+axis off;
+rectangle('Position',[0,0,100,100],'EdgeColor','black','LineWidth',4);
+rectangle('Position',[45,45,55,55],'EdgeColor','black','LineWidth',4);
+rectangle('Position',[0,0,45,45],'FaceColor','red','EdgeColor','black','LineWidth',4);
+rectangle('Position',[90,0,10,45],'EdgeColor','black','LineWidth',4);
+rectangle('Position',[90,25,10,20],'FaceColor','blue','EdgeColor','black','LineWidth',4);
+
+
+
 %Composition with Read, Blue, and Yellow
 
-figure;
+figure('Name','Composition with Read, Blue, and Yellow');
+axis off;
 rectangle('Position',[0,0,15,50],'EdgeColor','black','LineWidth',4);
 rectangle('Position',[15,0,45,45],'EdgeColor','black','LineWidth',4);
 rectangle('Position',[0,35,15,15],'EdgeColor','black','LineWidth',4);
